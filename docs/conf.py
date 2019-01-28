f5a9ef071a6e449e9a56ec06dea463d9437b6a20
amnahsiddique <amnah.siddique@trigsoft.com>
2019-01-28 13:32:56 +0500

    conf simple

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ release = u''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [`sphinx.ext.autodoc`
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -66,7 +66,7 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = `sphinx`
+pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -74,7 +74,7 @@ pygments_style = `sphinx`
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
